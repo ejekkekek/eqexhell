9b7f1c4f2c2830225650cec1cfc30773eb11b6ce
ejekkekek <98700127+ejekkekek@users.noreply.github.com>
2022-04-19 01:05:34 +0300

    Update Reflexaskima.lua

--- Reflexaskima.lua
+++ Reflexaskima.lua
@@ -5,26 +5,26 @@ gg.searchNumber("2,200,101", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
 bbbbbbbbu = gg.getResults(1000)
 gg.editAll("12212201", gg.TYPE_DWORD)
 gg.clearResults()
-qmnb = {{["memory"] = 32},{["name"] = "D-nilll Search 137002"},{["value"] = 1400129,["type"] = 4},{["lv"] = 519,["offset"] = 4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 1400129,["type"] = 4},{["lv"] = 519,["offset"] = 4,["type"] = 4}}
 qmxg = {{["value"] = 1400782,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
 gg.clearResults()
-qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501001,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 501001,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
 qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
-qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501002,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 501002,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
 qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
-qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501003,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 501003,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
 qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
-qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501004,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 501004,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
 qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
-qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501005,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 501005,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
 qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
-qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501006,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmnb = {{["memory"] = 32},{["name"] = "𝑏𝑦 𝑒𝑞𝑒𝑥ℎ𝑒𝑙𝑙"},{["value"] = 501006,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
 qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
 xqmnb(qmnb)
 gg.clearResults()
