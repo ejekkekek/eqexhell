deda46b34ee704f35f3bf2eccdde3d8fe7fb1311
ejekkekek <98700127+ejekkekek@users.noreply.github.com>
2022-04-18 23:13:06 +0300

    Create Reflexaskima.lua

---
+++ Reflexaskima.lua
@@ -0,0 +1,29 @@
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("2,200,101", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+bbbbbbbbu = gg.getResults(1000)
+gg.editAll("12212201", gg.TYPE_DWORD)
+gg.clearResults()
+qmnb = {{["memory"] = 32},{["name"] = "D-nilll Search 137002"},{["value"] = 1400129,["type"] = 4},{["lv"] = 519,["offset"] = 4,["type"] = 4}}
+qmxg = {{["value"] = 1400782,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+gg.clearResults()
+qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501001,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501002,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501003,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501004,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501005,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+qmnb = {{["memory"] = 32},{["name"] = "U-Search 135002"},{["value"] = 501006,["type"] = 4},{["lv"] = 501,["offset"] = -4,["type"] = 4}}
+qmxg = {{["value"] = 1501003051,["offset"] = 0,["type"] = 4}}
+xqmnb(qmnb)
+gg.clearResults()
